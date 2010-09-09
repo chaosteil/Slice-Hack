17ef16f931cca59efae8636b657fe0082e710227
Dominykas Djačenka <chaosteil@gmail.com>
2010-09-09 16:47:26 +0300

    Added some game map implementation.

---
+++ cpp/server/src/logic/game_map/gamemap.cpp
@@ -0,0 +1,11 @@
+#include "logic/game_map/gamemap.h"
+
+namespace slice_hack {
+namespace game_map {
+
+GameMap::GameMap() {}
+
+GameMap::~GameMap() {}
+
+}  // namespace game_map
+}  // namespace slice_hack
--- cpp/server/src/logic/game_map/gamemap.h
+++ cpp/server/src/logic/game_map/gamemap.h
@@ -1,15 +1,30 @@
 #ifndef SLICE_HACK_LOGIC_GAME_MAP_GAMEMAP_H_
 #define SLICE_HACK_LOGIC_GAME_MAP_GAMEMAP_H_
 
+#include <map>
+
+#include "logic/game_map/position.h"
+
 namespace slice_hack {
 namespace game_map {
 
+namespace entities {
+class Entity;
+}  // namespace entitites
+
 class GameMap {
  public:
+  enum ManagedEntity {
+    kManagedEntity = 0,
+    kUnmanagedEntity = 1
+  };
+
   GameMap();
   ~GameMap();
 
-  void Init(int width, int height);
+ private:
+  std::map<Position, entities::Entity *> entities_;
+  std::map<entities::Entity *, ManagedEntity> managed_entities_;
 };
 
 }  // namespace game_map
