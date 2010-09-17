a34ced7fbcd0e924e925d8cd8d92512394039317
Dominykas Djačenka <chaosteil@gmail.com>
2010-09-17 13:46:46 +0300

    Fixed up randomizer to actually populate the world.

--- cpp/server/src/logic/game_map/gamemaprandomizer.cpp
+++ cpp/server/src/logic/game_map/gamemaprandomizer.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "logic/game_map/gamemaprandomizer.h"
 
 #include <boost/random/mersenne_twister.hpp>
@@ -7,7 +8,9 @@
 namespace slice_hack {
 namespace game_map {
 
-GameMapRandomizer::GameMapRandomizer() {}
+GameMapRandomizer::GameMapRandomizer()
+    : manager_(NULL), width_(0), height_(0),
+      section_width_(0), section_height_(0) {}
 
 GameMapRandomizer::~GameMapRandomizer() {}
 
@@ -19,27 +22,28 @@ void GameMapRandomizer::RandomizeTerrain(
   width_ = width;
   height_ = height;
   section_width_ = section_width;
-  section_height_ = section_height_;
+  section_height_ = section_height;
 
   // 1. Lay down grass foundation
   BuildFoundation();
   // 2. Add a random amount of dirt everywhere
+  GenerateDirt();
   // 4. Add some objects
   // 5. Add water
   // 6. Add dirt roads
-  // 7. Add spawnpoint
-  // 8. Add dirt around water
+  // 7. Add dirt around water
+  // 8. Add spawnpoint
   // 9. Fix corners
   // DONE
 }
 
 void GameMapRandomizer::BuildFoundation() {
-  for (int y = height_; y < height_; y++) {
-    for (int x = width_; x < width_; x++) {
+  for (int y = 0; y < height_; y++) {
+    for (int x = 0; x < width_; x++) {
       GameMapSection *section =
         manager_->GetSectionFromPosition(Position(x, y));
-      for (int sec_y = section_height_; sec_y < section_height_; sec_y++) {
-        for (int sec_x = section_width_; sec_x < section_width_; sec_x++) {
+      for (int sec_y = 0; sec_y < section_height_; sec_y++) {
+        for (int sec_x = 0; sec_x < section_width_; sec_x++) {
           section->SetTerrain(Position(sec_x, sec_y), GameMapSection::kGrass);
         }
       }
@@ -49,13 +53,13 @@ void GameMapRandomizer::BuildFoundation() {
 
 void GameMapRandomizer::GenerateDirt() {
   boost::mt19937 gen;
-  int amount = width_ * height_ * section_height_ * section_width_;
+  int amount = (width_ * height_ * section_height_ * section_width_) / 10;
 
   for (int i = 0; i < amount; i++) { 
     int x = gen() % (width_ * section_width_);
     int y = gen() % (height_ * section_height_);
 
-    Position section_pos(x, y);
+    Position section_pos(0, 0);
     Position pos(x, y);
     manager_->TranslatePosition(&section_pos, &pos);
     
